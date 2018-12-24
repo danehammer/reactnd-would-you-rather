61aaf8a4dc5b8f3bfabd012431f90b75c3043df0
Dane Hammer <dane.molotok@gmail.com>
2018-12-24 17:29:04 -0600

    improve: show authedUser's answered and unanswered questions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,24 +3,59 @@ import {connect} from 'react-redux'
 import Question from './Question'
 
 export class Dashboard extends Component {
+
+  state = {
+    showAnswered: false
+  }
+
+  handleChange = (e) => {
+    this.setState({showAnswered: e.target.id === 'answered'})
+  }
+
   render() {
+    const {authedUser, questionIds} = this.props
+    const {showAnswered} = this.state
+    const {answers} = authedUser
+
     return (
       <div>
         <h3>Dashboard</h3>
+        <input
+          type='radio'
+          name='questions'
+          id='unanswered'
+          checked={!showAnswered}
+          onChange={this.handleChange}
+        />
+        <label>Unanswered</label>
+        <input
+          type='radio'
+          name='questions'
+          id='answered'
+          checked={showAnswered}
+          onChange={this.handleChange}
+        />
+        <label>Answered</label>
         <ul>
-          {this.props.questionIds.map((qid) => (
-          <li key={qid}>
-            <Question id={qid}/>
-          </li>
-          ))}
+          {questionIds.map((qid) => {
+            const hasAnswered = Object.keys(answers).includes(qid)
+            if (hasAnswered === showAnswered) {
+              return (
+                <li key={qid}>
+                  <Question id={qid}/>
+                </li>
+              )
+            }
+          })}
         </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps({questions}) {
+function mapStateToProps({authedUser, users, questions}) {
   return {
+    authedUser: users[authedUser],
     questionIds: Object.keys(questions).sort((a,b) => (
       questions[b].timestamp - questions[a].timestamp
   ))}
