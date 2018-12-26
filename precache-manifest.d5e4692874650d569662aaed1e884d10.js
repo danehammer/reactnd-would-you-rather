de4c34c140058bfaef57d0de641916d035d436f2
Dane Hammer <dane.molotok@gmail.com>
2018-12-25 22:12:51 -0600

    Updates

---
+++ precache-manifest.d5e4692874650d569662aaed1e884d10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a4d43c2f683aca9b52c",
+    "url": "/reactnd-would-you-rather/static/js/runtime~main.8a4d43c2.js"
+  },
+  {
+    "revision": "08c8f9db9dab0ef7dad6",
+    "url": "/reactnd-would-you-rather/static/js/main.08c8f9db.chunk.js"
+  },
+  {
+    "revision": "d09ee774a97a1f12ce7e",
+    "url": "/reactnd-would-you-rather/static/js/1.d09ee774.chunk.js"
+  },
+  {
+    "revision": "08c8f9db9dab0ef7dad6",
+    "url": "/reactnd-would-you-rather/static/css/main.bc60191d.chunk.css"
+  },
+  {
+    "revision": "beb680210a95e07dc70e93bd55450279",
+    "url": "/reactnd-would-you-rather/index.html"
+  }
+];
